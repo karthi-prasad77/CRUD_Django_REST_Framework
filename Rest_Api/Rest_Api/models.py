9ba515426844156482d6307e5b48a27eba495b58
karthi-prasad77 <karthiprasad44@gmail.com>
2023-01-21 06:44:55 +0530

    Modify models

--- Rest_Api/Rest_Api/models.py
+++ Rest_Api/Rest_Api/models.py
@@ -5,3 +5,6 @@ class Drink(models.Model):
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=500)
     
+
+    def __str__(self):
+        return self.name + " " + self.description
--- Rest_Api/Rest_Api/settings.py
+++ Rest_Api/Rest_Api/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
     'Rest_Api',
     'django.contrib.admin',
     'django.contrib.auth',
